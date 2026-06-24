7237ec187d422d982be1d922edda04dddbffd397
Venkatrao <venkatammu@Venkatraos-MacBook-Air.local>
2026-06-24 21:31:04 +0530

    dialog changes

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HeaderModel.java
@@ -0,0 +1,14 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+public interface HeaderModel {
+    
+    public String getMobileNumber();
+
+    public String getEmail();
+
+    public String getLoginText();
+
+    public String getRegisterText();
+
+    public String getUniversityName();
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/HeaderModelImpl.java
@@ -0,0 +1,37 @@
+package com.adobe.aem.guides.wknd.core.models.impl;
+
+import com.adobe.aem.guides.wknd.core.models.HeaderModel;
+
+public class HeaderModelImpl implements HeaderModel{
+
+    @Override
+    public String getMobileNumber() {
+
+        return null;
+    }
+
+    @Override
+    public String getEmail() {
+
+       return null;
+    }
+
+    @Override
+    public String getLoginText() {
+    
+        return null;
+    }
+
+    @Override
+    public String getRegisterText() {
+
+        return null;
+    }
+
+    @Override
+    public String getUniversityName() {
+
+        return null;
+    }
+    
+}
