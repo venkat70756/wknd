9c75f297d3f9922b27d6e71112c5ee5cd6bf9413
Venkatrao <venkatammu@Venkatraos-MacBook-Air.local>
2026-07-09 08:06:12 +0530

    loggers

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/DemoModelImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/DemoModelImpl.java
@@ -9,12 +9,16 @@ import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.adobe.aem.guides.wknd.core.models.DemoModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Model(adaptables = Resource.class,
     adapters = DemoModel.class,
     defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
 public class DemoModelImpl implements DemoModel{
+
+    private static final Logger log = LoggerFactory.getLogger(DemoModelImpl.class);
 
     @ValueMapValue
     private String id;
@@ -38,6 +42,14 @@ public class DemoModelImpl implements DemoModel{
 
     @Override
     public String getHeading(){
+//        log.trace("TRACE");
+//        log.debug("Debug");
+//        log.info("Info");
+//        log.warn("Warn");
+//        log.error("Error");
+
+
+        log.info("Heading : {}", heading);
         return heading;
     }
 
