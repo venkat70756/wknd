1e16bdfce94c981cbf1a13eb9b92d3086eec7a9e
Venkatrao <venkatammu@Venkatraos-MacBook-Air.local>
2026-07-14 07:50:37 +0530

    OSGI Configuration

--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/TestConfiguration.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/TestConfiguration.java
@@ -3,6 +3,7 @@ package com.adobe.aem.guides.wknd.core.services.impl;
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.osgi.service.metatype.annotations.Option;
 
 @ObjectClassDefinition(name = "Test Configuration for Phalguni", description = "Entering the Employee details")
 public @interface TestConfiguration {
@@ -14,7 +15,22 @@ public @interface TestConfiguration {
     public int getEmployeeID() default 2443;
 
     @AttributeDefinition(name = "Is Contract Employee", type = AttributeType.BOOLEAN, description = "If the employes type is Contract, please enable the checkbox, if not uncheck the checkbox ")
-    public boolean isContractEmployee() default true;
+    public boolean getIsContractEmployee() default true;
+
+    @AttributeDefinition(name = "Employee Skills", type = AttributeType.STRING, description = "Enter the employee skills")
+    public String[] getEmployeeSkills() default {"HTML", "JavaScript"};
+
+    @AttributeDefinition(name = "Employee Location", type = AttributeType.STRING, description = "Select the Employee Location", options = {
+            @Option(label = "India", value = "in"),
+            @Option(label = "United States", value = "usa"),
+            @Option(label = "United Kingdom", value = "UK")
+    })
+    public String getEmployeeLocation();
+
+
+    @AttributeDefinition(name = "Employees Id", type = AttributeType.PASSWORD, description = "Enter the Employeee Secret ID")
+    public String getEmployeeSecretID();
+
 }
 
 
--- core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/TestServiceImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/impl/TestServiceImpl.java
@@ -9,22 +9,47 @@ import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.security.PrivateKey;
+import java.util.Arrays;
+
 @Component(service = TestService.class, immediate = true)
 @Designate(ocd = TestConfiguration.class)
 public class TestServiceImpl implements TestService {
 
     private static final Logger log = LoggerFactory.getLogger(TestServiceImpl.class);
 
+    private String eName;
+    private int eId;
+    private boolean eType;
+    private String[] eSkills;
+    private String eLocation;
+    private String ePassword;
+
+
 
     @Activate
-    public void activate() {
-        log.info("Activating TestServiceImpl");
+    @Modified
+    public void config(TestConfiguration  testConfiguration) {
+//        log.info("Activating TestServiceImpl");
+        eName = testConfiguration.getEmployeeName();
+        eId = testConfiguration.getEmployeeID();
+        eType = testConfiguration.getIsContractEmployee();
+        eSkills = testConfiguration.getEmployeeSkills();
+        eLocation = testConfiguration.getEmployeeLocation();
+        ePassword = testConfiguration.getEmployeeSecretID();
+
+        log.info("Employee Name : " + eName);
+        log.info("Employee ID : " + eId);
+        log.info("Employee Type : " + eType);
+        log.info("Employee Skills : " + Arrays.toString(eSkills));
+        log.info("Employee Location : " + eLocation);
+        log.info("Employee Password : " + ePassword);
     }
 
-    @Modified
-    public void modified() {
-        log.info("Modified TestServiceImpl");
-    }
+//    @Modified
+//    public void modified() {
+//        log.info("Modified TestServiceImpl");
+//    }
 
     @Deactivate
     public void deactivate() {
