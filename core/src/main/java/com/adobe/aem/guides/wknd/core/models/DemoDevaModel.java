3ca05318e475cc505bb1837478b0f164488c20c2
venkat70756 <venkatyvry1234@gmail.com>
2026-08-09 08:12:39 +0530

    sling models

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/DemoDevaModel.java
@@ -0,0 +1,12 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+
+public interface DemoDevaModel {
+
+    public String getBannerTitle();
+
+    public String getBannerDescription();
+
+    public String getBannerImage();
+
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Impl/DemoDevaModelImpl.java
@@ -0,0 +1,47 @@
+package com.adobe.aem.guides.wknd.core.models.Impl;
+
+import com.adobe.aem.guides.wknd.core.models.DemoDevaModel;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Required;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+
+@Model(adaptables = Resource.class,
+        adapters = DemoDevaModel.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class DemoDevaModelImpl implements DemoDevaModel {
+
+
+    @ValueMapValue
+    private String bannerTitle;
+
+
+    @ValueMapValue
+    private String bannerDescription;
+
+
+    @ValueMapValue
+    private String bannerImage;
+
+
+
+
+    @Override
+    public String getBannerTitle() {
+        bannerTitle = bannerTitle.toUpperCase();
+        return bannerTitle;
+    }
+
+    @Override
+    public String getBannerDescription() {
+        return bannerDescription;
+    }
+
+    @Override
+    public String getBannerImage() {
+        return bannerImage;
+    }
+}
